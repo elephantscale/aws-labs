fa3a98864380c5093ce643c38fe1ed80dc77b7e7
Sujee Maniyam (sujee-mac2) <sujee@sujee.net>
2016-11-26 10:05:28 -0800

    first commit - login

---
+++ launch-ec2-instance.md
@@ -0,0 +1,20 @@
+<link rel='stylesheet' href='assets/main.css'/>
+
+[<< back to main index](README.md) 
+
+---
+
+# Launch EC2 Instance
+
+### Overview
+Login and access AWS console
+
+### Depends On 
+None
+
+### Run time
+10 mins
+
+
+## Step 1 : Obtain login details
+Instructor will provide these
---
+++ scripts/generate-students.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+## -- start config
+
+profile=es-training-sujee
+region=us-west-2
+
+# if you don't have pwgen installed, just set a password manually here, just alpha-numeric chars please (no special chars like $ ! ~)
+password="xky090ljXvU"
+password=$(pwgen -n -s 14 1)
+
+group_name=students
+
+## -- end config
+
+
+#echo "using pasword : $password"
+
+
+# generate a few users
+for i in {1..10}
+do
+      student="student$i"
+        # create user
+        aws --profile "$profile" \
+        iam create-user --user-name "$student"
+
+        # change login
+        aws --profile "$profile" \
+        iam create-login-profile --no-password-reset-required  --user-name "$student"  --password "${password}"
+
+        # add to student group
+        aws --profile "$profile" \
+        iam add-user-to-group --user-name "$student" --group-name "$group_name"
+
+        echo "== $i :  user = $student   password = $password"
+
+done
