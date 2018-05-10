f4dc2c73d6df14f6d9afec0504dde515405fbfc7
Mark Kerzner <mark@elephantscale.com>
2018-05-09 21:15:48 -0700

    script fixed

--- scripts/list-instances.sh
+++ scripts/list-instances.sh
@@ -1,5 +1,5 @@
 for region in `aws ec2 describe-regions --output text | cut -f3`
 do
      echo -e "\nListing Instances in region:'$region'..."
-     aws ec2 describe-instance-status --region $region --instance-state-code 16
+     aws ec2 describe-instance-status --region $region --filters Name=instance-state-code,Values=16
 done
